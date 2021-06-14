acdaf478be565a2f894079f576620a5226a39e07
mohith <mohithdarkrider@gmail.com>
2021-06-14 18:31:59 +0100

    Add files via upload
    finally code with fully implemented data extraction and simulation

--- BCI_jup.py
+++ BCI_jup.py
@@ -1,26 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
-import pynput
 import tensorflow as tf
 import keypress
 import numpy as np
 import pandas as pd
-import brainflow as bf
 import time
 import csv
 from pylsl import StreamInlet, resolve_stream
 
 
-# In[12]:
-
-
-dataset = pd.read_csv("openbci2.csv",skiprows=5)
-X = dataset.iloc[:,1:8].values
-y = dataset.iloc[:,8].values
+dataset = pd.read_csv("MI_data0.csv")
+X = dataset.iloc[:, :].values
+y = dataset.iloc[:, :].values
 
 
 # In[4]:
@@ -76,19 +65,12 @@ classifier.fit(X_train, y_train, batch_size = 100, epochs = 50)
 y_pred = classifier.predict(X_test)
 
 
-# In[ ]:
-
-
 '''y_lmao = y_pred
 for x in range(len(y_pred[:,1])):
    ree = np.argmax(y_lmao[x])
    y_lmao[x] = [0,0,0,0]
    y_lmao[x,ree] = 1
 '''
-
-
-# In[57]:
-
 
 '''
 print(y_lmao)
@@ -107,87 +89,59 @@ for x in range(len(y_pred[:,1])):
 #print(correct)
 print((correct/total)*100)
 '''
-
-
-# In[25]:
-
+print("starting...")
+streams = resolve_stream('type', 'EEG')
+inlet = StreamInlet(streams[0])
+duration = 0.5
 
 
 def predict():
-    print("starting...")
-    streams = resolve_stream('type', 'EEG')
-    inlet = StreamInlet(streams[0])
-    duration = 0.5
-
-   
-    def testLSLSamplingRate():
+    def sample_extract():
         start = time.time()
-
         with open('openbci.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             while time.time() <= start + duration:
                 samples, timestamp = inlet.pull_chunk()
                 if samples:
-                    for sample in samples:
-                        writer.writerows(samples)
+                    writer.writerows(samples)
 
-                        
     def processing_data():
         with open('prediction_data.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
-            for sam in range(0, 20): #increase the range to get more recordings
-                testLSLSamplingRate()
-                dataset = pd.read_csv("openbci.csv")  
-                raw = dataset.iloc[:, :].values
+            for sam in range(0, 21):  # increase the range to get more recordings
+                sample_extract()
+                if sam == 0:
+                    continue
+                raw_dataset = pd.read_csv("openbci.csv")
+                raw = raw_dataset.iloc[:, :].values
                 print(len(raw))
-                X = []
-                for y in range(len(raw[0])):
-                    X.append(np.average(raw[:, y]))
-                print(X)
-                writer.writerow([X[0], X[1], X[2], X[3], X[4], X[5], X[6], X[7]])
+                p = []
+                for r in range(len(raw[0])):
+                    p.append(np.average(raw[:, r]))
+                print(p)
+                writer.writerow([p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7]])
 
-                  
     processing_data()
 
-   
-test = pd.read_csv("prediction_data.csv")
-lol = test.iloc[:, :].values
-labelt = LabelEncoder()
-actual = labelt.fit_transform(actual)
-actual = actual.reshape(-1, 1)
-ohet = OneHotEncoder()
-actual = ohet.fit_transform(actual).toarray()
-lol = sctr.transform(lol)
-prediction = classifier.predict(lol)
-    #print(prediction)
-ree = np.argmax(prediction[0])
-prediction[0] = [0,0,0,0]
-prediction[0,ree] = 1
-time.sleep(1.5)
-if ree == 0:
-    keypress.press('d')
-elif ree == 1:
-    keypress.press('a')
-elif ree == 2:
-    keypress.press('w')
-elif ree == 3:
-    keypress.press('s')
-time.sleep(1.5)
-
-
-
-
-# In[4]:
+    test = pd.read_csv("prediction_data.csv")
+    lol = test.iloc[:, :].values
+    lol = sctr.transform(lol)
+    prediction = classifier.predict(lol)
+    ree = np.argmax(prediction[0])
+    prediction[0] = [0, 0, 0, 0]
+    prediction[0, ree] = 1
+    time.sleep(1.5)
+    if ree == 0:
+        keypress.press('d')
+    elif ree == 1:
+        keypress.press('a')
+    elif ree == 2:
+        keypress.press('w')
+    elif ree == 3:
+        keypress.press('s')
+    time.sleep(1.5)
 
 
 while 1:
     predict()
-    con = int(input("do want to do it again? if so press 1 else press anything else")) #just for testing needs to be changed
-    if con != 1:
-        break
-print("end of simulation")
-
-
-
-
 
