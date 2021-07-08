8afe8feddc29cf26421cc5ac0f8ebdaa48bed019
mohith <mohithdarkrider@gmail.com>
2021-07-08 02:11:41 +0100

    final commit

--- bci_jup.py
+++ bci_jup.py
@@ -1,4 +1,3 @@
-import keras.optimizers
 import tensorflow as tf
 import keypress
 import numpy as np
@@ -8,23 +7,25 @@ import csv
 from pylsl import StreamInlet, resolve_stream
 import statistics
 import sklearn.metrics as met
-
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split
+# Reading the dataset
 dataset = pd.read_csv("MI_data_6.csv")
 X = dataset.iloc[:, :24].values
 y = dataset.iloc[:, -1].values
 print(y[400], y[3000], y[5000])
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+# encoding the labels
 label = LabelEncoder()
 y = label.fit_transform(y)
 y = y.reshape(-1, 1)
 ohe = OneHotEncoder()
 y = ohe.fit_transform(y).toarray()
 print(y[400], y[3000], y[5000])
-from sklearn.model_selection import train_test_split
+# hold _out validation split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 classifier = tf.keras.models.Sequential()
-
+# hidden layers
 classifier.add(tf.keras.layers.Dense(units=29, activation='sigmoid'))
 
 classifier.add(tf.keras.layers.Dense(units=29, activation='sigmoid'))
@@ -34,14 +35,13 @@ classifier.add(tf.keras.layers.Dense(units=29, activation='sigmoid'))
 classifier.add(tf.keras.layers.Dense(units=29, activation='sigmoid'))
 
 classifier.add(tf.keras.layers.Dense(units=29, activation='sigmoid'))
-
-#classifier.add(tf.keras.layers.Dropout(0.9))
 
 classifier.add(tf.keras.layers.Dense(units=3, activation='softmax'))
 
-classifier.compile(optimizer='nadam', loss='categorical_crossentropy', metrics=['accuracy'])
-
+classifier.compile(optimizer='RMSprop', loss='hinge', metrics=['accuracy'])
+# training
 classifier.fit(X_train, y_train, batch_size=50, epochs=200)
+# using hold out validation to test the performance
 y_pred = classifier.predict(X_test)
 final = []
 tes = []
@@ -54,9 +54,10 @@ for d in range(len(final)):
     if final[d] == tes[d]:
         con += 1
 print("test accuracy is", (con*100)/len(final))
+# using an different dataset to test the true performance
 test = pd.read_csv("MI_data_5.csv")
 lol = test.iloc[:, :24].values
-y2 = dataset.iloc[:, -1].values
+y2 = test.iloc[:, -1].values
 label2 = LabelEncoder()
 y2 = label2.fit_transform(y2)
 y2 = y2.reshape(-1, 1)
@@ -76,7 +77,7 @@ for d in range(len(final)):
 print("test accuracy is", (con*100)/len(final))
 matrix = met.confusion_matrix(prediction.argmax(axis=1), y2.argmax(axis=1))
 print(matrix)
-
+# code for character control
 time.sleep(5)
 print("starting...")
 streams = resolve_stream('type', 'EEG')
